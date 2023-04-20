ec677bc556ae6d297a5b04082672cc7cc82ef187
kashif87713 <kjabbar256@gmail.com>
2023-04-20 14:48:01 +0500

    comment

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,15 @@
 import '@/styles/globals.css'
 import Navbar from '../Components/Navbar/Navbar'
 import Footer from '../Components/Footer/Footer'
-import { createTheme, ThemeProvider } from '@mui/material';
 
 export default function App({ Component, pageProps }) {
-    const theme = createTheme();
 
     return (
-        <ThemeProvider theme={theme}>
+        <>
             <Navbar />
             <Component {...pageProps} />
             <Footer />
-        </ThemeProvider>
+        </>
     )
 
 }

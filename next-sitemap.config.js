8a4c31faf918073cb0ba7f6c0a5fa871f98e6fda
arslan456 <muhammadarslanjabbar@gmail.com>
2023-05-30 20:50:03 +0500

    'sitemap-added'

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: 'https://www.greenclothing.co/', // Replace with your website URL
+  generateRobotsTxt: true, // Optional: Generate a robots.txt file
+  exclude: ['/admin', '/admin/*'], // Optional: Exclude specific paths from the sitemap
+};
